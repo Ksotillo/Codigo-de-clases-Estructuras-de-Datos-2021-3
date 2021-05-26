438141f7339fc1ffaf17cbbda100be247f964d6c
Kevin <ksotillo@gmail.com>
2021-05-26 10:49:17 -0400

    Deleted something unnecessary

--- Semana 6/Clase_26_05_2021/src/main/Node.java
+++ Semana 6/Clase_26_05_2021/src/main/Node.java
@@ -63,47 +63,6 @@ public class Node<T> {
         return 0;
     }
     
-    public static int comparator(Node firstNode, Node secondNode) {
-        String firstClass = firstNode.getData().getClass().getSimpleName();
-        String secondClass = firstNode.getData().getClass().getSimpleName();
-        if (!firstClass.equals(secondClass)) {
-            throw new Error("Nodes does not have same data");
-        }
-        
-        switch(firstClass) {
-            case "Integer":
-                Integer firstDataInteger = (Integer)firstNode.getData();
-                Integer secondDataInteger = (Integer)secondNode.getData();
-                if (firstDataInteger > secondDataInteger) {
-                    return 1;
-                } else if (secondDataInteger > firstDataInteger) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            case "Double":
-                Double firstDataDouble = (Double)firstNode.getData();
-                Double secondDataDouble = (Double)secondNode.getData();
-                if (firstDataDouble > secondDataDouble) {
-                    return 1;
-                } else if (secondDataDouble > firstDataDouble) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            case "String":
-                String firstDataString = (String)firstNode.getData();
-                String secondDataString = (String)secondNode.getData();
-                if (firstDataString.length() > secondDataString.length()) {
-                    return 1;
-                } else if (secondDataString.length() > firstDataString.length()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-        }
-        return 0;
-    }
     
     
     
